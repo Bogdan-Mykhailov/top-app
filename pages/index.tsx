9e936f411eb23ab55935489ae853320f7f989a79
bogdan_mykhailov <bogdan_mykhailov@icloud.com>
2022-12-21 21:04:33 +0200

    :bug: need to fix bud with alias.tsx have problem with build

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import React, {useState} from "react";
 import {Button, HTag, Paragraph, Rating, Tag} from "../components";
 import {WithLayout} from "../layout/WithLayout";
+import {GetStaticProps} from "next";
+import axios from "axios";
+import {IMenuItem} from "../interfaces/menu.interface";
 
-function Home(): JSX.Element {
+function Home({ menu }: IHomeProps): JSX.Element {
   const [rating, setRating] = useState<number>(4);
 
   return (
@@ -17,8 +20,30 @@ function Home(): JSX.Element {
       <Tag size='large'>Text</Tag>
       <Tag color='gray'>Text</Tag>
       <Rating rating={rating} setRating={setRating} isEditable/>
+      <ul>
+        {menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
+
+      </ul>
     </>
   );
 }
 
 export default WithLayout(Home);
+
+export const getStaticProps: GetStaticProps<IHomeProps> = async () => {
+  const firstCategory = 0;
+  const {data: menu} = await axios.post<IMenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+    firstCategory
+  })
+  return {
+    props: {
+      menu,
+      firstCategory,
+    }
+  };
+};
+
+interface IHomeProps extends Record<string, unknown>{
+  menu: IMenuItem[];
+  firstCategory: number
+}
